986d007bb0ee2c3c258e52428279de277f219259
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-02-14 14:51:17 +0100

    use the full deployment conf when patching

--- update-existing-deployment.sh
+++ update-existing-deployment.sh
@@ -7,16 +7,10 @@ source `dirname $0`/extract-artifact-meta.sh ${repoName}
 # This way VVP will suspend & start the flink job upon a PATCH action
 commitHash=`echo ${buildVersion} | cut -c 1-7`
 
-curl -X PATCH "http://localhost:8080/api/v1/namespaces/${vvpNamespace}/deployments/${deploymentId}" \
-    -H "Authorization: Bearer ${vvpAPIToken}" \
-    -H "accept: application/yaml" -H "Content-Type: application/yaml" -s -d "
-kind: Deployment
-apiVersion: v1
-spec:
-  state: RUNNING
-  template:
-    spec:
-      artifact:
-        jarUri: >-
-          wasbs://${blobContainer}@${storageAccount}.blob.core.windows.net/artifacts/namespaces/${vvpNamespace}/${jarFileName}?commit=${commitHash}
+eval "
+curl -X PATCH \"http://localhost:8080/api/v1/namespaces/${vvpNamespace}/deployments\" \
+    -H \"Authorization: Bearer ${vvpAPIToken}\" \
+    -H \"accept: application/yaml\" -H \"Content-Type: application/yaml\" -s -d \"
+`echo \"${vvpDeploymentConf}\"`
+\"
 "
