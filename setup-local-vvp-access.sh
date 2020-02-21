f23c75c05bbf8d5ce01ee6c48b2f9f5efa2be69a
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-02-21 16:35:45 +0100

    Try with UPPERCASE variable

--- setup-local-vvp-access.sh
+++ setup-local-vvp-access.sh
@@ -1,9 +1,9 @@
 
 # az login
-az login --service-principal -u ${servicePrincipal} -p ${servicePrincipalKey} --tenant ${tenantId}
+az login --service-principal -u ${SERVICEPRINCIPAL} -p ${SERVICEPRINCIPALKEY} --tenant ${TENANTID}
 
 # get cluster credential
-az aks get-credentials --resource-group ${resourceGroup} --name ${kubernetesCluster}
+az aks get-credentials --resource-group ${RESOURCEGROUP} --name ${KUBERNETESCLUSTER}
 
 # port forward
-kubectl port-forward service/vvp-ververica-platform 8080:80 --namespace ${kubernetesNamespace} &
+kubectl port-forward service/vvp-ververica-platform 8080:80 --namespace ${KUBERNETESNAMESPACE} &
